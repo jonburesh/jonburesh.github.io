131f3ede7091a79be212cc49f02bf361747768ea
jon_buresh <bojanglesjb@gmail.com>
2019-04-26 18:03:31 -0700

    page indicator - cleanup - header collapse on smaller screens

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 var sitePages;
 var pageCount;
-
+var currentPageButton;
 
 function getPages()
 {
     sitePages = document.getElementsByClassName("site-page");
+    currentPageButton = document.getElementsByClassName("current-page");
     pageCount = sitePages.length;
 }
 
@@ -17,26 +18,33 @@ function setupCards()
         // set to expanded
         cardTarget.toggleClass('expanded');
         // set to fullyExpanded after short delay
-        setTimeout(function() 
+        setTimeout(function()
         { 
             cardTarget.toggleClass('fullyExpanded'); 
         }, 50);
     });
 }
 
-function pageSelected(id)
-{
+function pageSelected(id, sender)
+{   
     for (i = 0; i < pageCount; i++) 
     {
         if (sitePages[i].id == id)
         {
-            sitePages[i].style.display = 'block';
+            sitePages[i].style.display = 'block';   
         }
         else
         {
             sitePages[i].style.display = 'none';
         }
     }
+    if (currentPageButton.id != sender.id)
+    {
+        $(currentPageButton).removeClass("current-page");
+    }
+    currentPageButton = sender;
+    $(sender).addClass("current-page");
+    
 }
 
 getPages();
